cff33b905ea5fa9f4e1a55bfa9a1f45eacaf7b6d
Liudmyla01 <74067187+Liudmyla01@users.noreply.github.com>
2021-01-26 15:04:16 +0200

    Delete spriny02 directory

--- spriny02/t04/js/script.js
+++
@@ -1,8 +0,0 @@
-
-function solver(a, b, c, d, e) {
-    return  Math.pow(a,2) - (5 * b * c) + (d / 3) + e ; 
-  }
-  alert (solver (40, -9, 0.5, 7, 100) .toFixed(2));
-//   alert (solver (40, "doesn't look like a number", 0.5, 7, 100));
-// alert (solver (40, -9, 0.5, 7));
-  
